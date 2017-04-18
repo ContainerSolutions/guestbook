0db2e445e900d123dd62970524594eacb782de9c
Carlos León <mail@carlosleon.info>
2017-04-18 11:55:37 +0200

    Exit program if connection string is not provider

--- main.go
+++ main.go
@@ -11,16 +11,20 @@ import (
 func main() {
 	sqlconn := flag.String("postgres", "", "connect string for postgres")
 	flag.Parse()
+	if len(*sqlconn) == 0 {
+		log.Fatal("connection string not provided")
+	}
+
 	r, err := db.New(*sqlconn)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//r := entries.Retriever(entries.Mocker{})
+
 	var a *api.Api
 	a, err = api.New(r)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Fatal(a.Serve())
 
+	log.Fatal(a.Serve())
 }
